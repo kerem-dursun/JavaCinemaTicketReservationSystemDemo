c399ec31b894858650a4aa0aea05a91a4bf00327
Kerem Dursun <keremdrsnnsrd@gmail.com>
2025-12-19 16:06:14 +0300

    Constructor and attiributes added.

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,14 @@
 public class Customer {
+    private int id ;
     private String userName;
     private String password;
+    private static int idCounter = 0;
+
+    public Customer(String userName, String password){
+        this.id = idCounter++;
+        this.userName = userName;
+        this.password = password;
+    }
+
 
 }
